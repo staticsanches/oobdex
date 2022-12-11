17fc6d0469b66a69b0c74307e739a4fa866d0b7c
Felipe Sanches <staticsanches@gmail.com>
2022-12-11 10:50:26 -0300

    Adjusting location card's title

--- lib/pages/locations_grid_page.dart
+++ lib/pages/locations_grid_page.dart
@@ -83,33 +83,37 @@ class _LocationCard extends HookWidget {
             ),
           ),
           Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  location.name.toString(),
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                const SizedBox(height: 15),
-                Text(
-                  appLocalizations.ooblets,
-                  style: Theme.of(context).textTheme.titleSmall,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    _LocationOobletsCaughtStatus(
-                      location: location,
-                      caught: true,
-                    ),
-                    const SizedBox(width: 20),
-                    _LocationOobletsCaughtStatus(
-                      location: location,
-                      caught: false,
-                    ),
-                  ],
-                ),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.only(top: 8, right: 8, bottom: 8),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    location.name.toString(),
+                    style: Theme.of(context).textTheme.titleMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                    appLocalizations.ooblets,
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _LocationOobletsCaughtStatus(
+                        location: location,
+                        caught: true,
+                      ),
+                      const SizedBox(width: 20),
+                      _LocationOobletsCaughtStatus(
+                        location: location,
+                        caught: false,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ],
